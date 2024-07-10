df44b1055398bc85e28a7a5f9623e7c6aee666f6
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-10 01:30:55 +0000

    learning javascript

--- Chapter-1/sandbox.js
+++ Chapter-1/sandbox.js
@@ -1,1 +1,40 @@
 
+// Variables //
+//let myAge = 30;
+//let year = 2023;
+//console.log(year,myAge);
+
+//age=(25);
+//console .log(age);
+
+
+// Constants  
+//const points = 100;  
+//console.log(points);
+
+
+
+// Javascript Data Types //
+
+// Strings
+console.log('hello, world');
+
+let email = "mariao@thenetninja.co.uk"
+console.log(email);
+
+
+// String Concatenation -- joining two strings together
+let firstName = "Marie"
+let lastName = "Ogundipe"
+let fullname = firstName + " " + lastName; 
+console.log(fullname);
+
+
+
+
+
+
+// Boolean
+// Undefined
+// Null
+// Object
