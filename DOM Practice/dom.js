e5c446944cd991db2cc6de1803aa6dc0d4378190
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-22 18:06:11 +0000

    learning javascript

--- DOM Practice/dom.js
+++ DOM Practice/dom.js
@@ -3,10 +3,12 @@
 
 // console.log(banner);
 
+
 // // query the dom by class
 // const classname = document.getElementsByClassName('title');
 
 // console.log(classname);
+
 
 // // query the dom by tagname
 // const tags = document.getElementsByTagName('li');
@@ -14,7 +16,24 @@
 // console.log(tags)
 
 
-const titles = document.getElementsByTagName('li');
-for(i=0; i <titles.length; i++){
-    console.log(titles[i]);
-}
+// // how to cycle through anhtml collection -- using loops
+// const titles = document.getElementsByTagName('li');
+// for(i=0; i <titles.length; i++){
+//     console.log(titles[i]);
+// }
+
+
+
+// // how to cycle through html elements which are'nt an array
+// const titles = document.getElementsByTagName('li');
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles))); // - convertst to an array
+
+
+// Array.from(titles).forEach(function(item){
+//     console.log(item);
+// });
+
+
+
