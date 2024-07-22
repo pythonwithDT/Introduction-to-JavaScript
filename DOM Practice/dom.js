e40159d30e3a001d00b1e481b77aef0bc4383102
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-22 17:41:12 +0000

    learning javascript

---
+++ DOM Practice/dom.js
@@ -0,0 +1,20 @@
+// // query the dom by id
+// const banner = document.getElementById('page-banner');
+
+// console.log(banner);
+
+// // query the dom by class
+// const classname = document.getElementsByClassName('title');
+
+// console.log(classname);
+
+// // query the dom by tagname
+// const tags = document.getElementsByTagName('li');
+
+// console.log(tags)
+
+
+const titles = document.getElementsByTagName('li');
+for(i=0; i <titles.length; i++){
+    console.log(titles[i]);
+}
