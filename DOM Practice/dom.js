c37de08e982ed4f31674595af6c507e2ae1a4e49
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-24 01:16:20 +0000

    learning javascript

--- DOM Practice/dom.js
+++ DOM Practice/dom.js
@@ -183,3 +183,21 @@ hideBox.addEventListener('change', function(e){
             }
 })
 
+
+// filter books
+
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1){
+            book.style.display = 'block';
+            } else{
+                book.style.display = 'none';
+                }
+            });
+});
+            
+
