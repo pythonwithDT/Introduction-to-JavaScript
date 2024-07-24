576c3bb620a5abf613c2a2b39fab7fd64cd1cef3
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-24 01:01:49 +0000

    learning javascript

--- DOM Practice/dom.js
+++ DOM Practice/dom.js
@@ -153,31 +153,33 @@ addform.addEventListener('submit', function(e){
     const value = addform.querySelector('input[type="text"]').value;
     
     // create elements
-
     const li = document.createElement('li');
     const bookName = document.createElement('span');
     const deleteBtn = document.createElement('span'); 
     
-    
     // add contnent 
-
     deleteBtn.textContent = 'delete';
     bookName.textContent = value;
 
-
     // add classes
-    
     bookName.classList.add('name');
     deleteBtn.classList.add('delete');
 
     // append to document
-
     li.appendChild(bookName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
-
-
 })
- 
 
+
+// hide books
+ 
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+    if(hideBox.checked){
+        list.style.display = 'none';
+        } else{
+            list.style.display = 'block';
+            }
+})
 
