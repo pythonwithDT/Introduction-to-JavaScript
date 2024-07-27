c2c878570615dc7ae42b2f872466673fb2f89d11
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-27 00:57:45 +0000

    learning javascript

--- Advance JS Features/sandbox.js
+++ Advance JS Features/sandbox.js
@@ -2,22 +2,29 @@
 // array = a variable like strucutre that can 
 //          hold more than one(1) value
 
-let fruits = ['apple', "orange", "banana"];
+let fruits = ['apple', "orange", "banana", 'coconut'];
 
-fruits[1] = 'grape';
+// fruits[1] = 'grape';
 
-fruits.push('coconut'); // - add an element to an array
+// fruits.push('coconut'); // - add an element to an array
 
-fruits.pop(); // remove last element
+// fruits.pop(); // remove last element
 
-fruits.unshift('mango') // - add an element to the begining 
+// fruits.unshift('mango') // - add an element to the begining 
 
-fruits.shift(); // - remove an element from the begining 
+// fruits.shift(); // - remove an element from the begining 
 
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
-console.log(fruits[3]);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+
+
+// let numofFruits = fruits.length;
+// console.log(numofFruits); // - print the number of elements in the array
+
+// let index = fruits.indexOf('orange');
+// console.log(index); // - print the index of the element in the array // - getting -1 means the element was not found 
 
 
 
@@ -25,3 +32,15 @@ console.log(fruits[3]);
 // for (let i = 0; i < fruits.length; i++) {
 //     console.log(fruits[i]);
 //     };
+
+// enhance for loop 
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+// sorting elements 
+fruits.sort();
+console.log(fruits); // - sort the array in ascending order
+// reverse the array
+fruits.reverse();
+console.log(fruits); // - sort the array in descending order
