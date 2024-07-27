64b704ad8c6d9bfdad7d33b685c0e8ae0aaafe15
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-27 01:31:46 +0000

    learning javascript

--- Advance JS Features/sandbox.js
+++ Advance JS Features/sandbox.js
@@ -54,31 +54,31 @@
 // console.log(myObj.name); // - print the value of the key 'name' in the object
 // console.log(myObj['name']); // - print the value of the key 'name' in the object using bracket notation
 
-const anotherObj = {
-    alive: true,
-    answer: 43,
-    hobbies: ['Eat', 'Sleep', 'Code'], // - you can have an array in an obj 
-    address: { // you can have a nested object in an object
-        street: '123 Main St',
-        city: 'Anytown',
-        state: 'CA',
-        zip: '12345'
-        },
-    action: function(){  // - u can have methods in an obj
-        return 'I am alive';
-    } ,
-    place: function(){  // - u can refer to an information inside a function in an obj
-        return `This is ${this.address.city}`;
-    }     
-}
-console.log(anotherObj);
-console.log(anotherObj.alive); // - print the value of the key 'alive'
-console.log(anotherObj['alive']); // - print the value of the key 'alive'    
-console.log(anotherObj.hobbies); // - print the value of the key 'hobbies
-console.log(anotherObj.address); // - print the value of the key 'address'
-console.log(anotherObj.address.street); // - print the value of the key 'street'
-console.log(anotherObj.address['city']); // - print the value of the key 'city
-console.log(anotherObj.hobbies[2]); // - prints out the index of in an array
-console.log(anotherObj.action()); // - prints out the value of the method in an object
-console.log(anotherObj.place()); // - prints out the value of the method in an object
+// const anotherObj = {
+//     alive: true,
+//     answer: 43,
+//     hobbies: ['Eat', 'Sleep', 'Code'], // - you can have an array in an obj 
+//     address: { // you can have a nested object in an object
+//         street: '123 Main St',
+//         city: 'Anytown',
+//         state: 'CA',
+//         zip: '12345'
+//         },
+//     action: function(){  // - u can have methods in an obj
+//         return 'I am alive';
+//     } ,
+//     place: function(){  // - u can refer to an information inside a function in an obj
+//         return `This is ${this.address.city}`;
+//     }     
+// }
+// console.log(anotherObj);
+// console.log(anotherObj.alive); // - print the value of the key 'alive'
+// console.log(anotherObj['alive']); // - print the value of the key 'alive'    
+// console.log(anotherObj.hobbies); // - print the value of the key 'hobbies
+// console.log(anotherObj.address); // - print the value of the key 'address'
+// console.log(anotherObj.address.street); // - print the value of the key 'street'
+// console.log(anotherObj.address['city']); // - print the value of the key 'city
+// console.log(anotherObj.hobbies[2]); // - prints out the index of in an array
+// console.log(anotherObj.action()); // - prints out the value of the method in an object
+// console.log(anotherObj.place()); // - prints out the value of the method in an object
 
