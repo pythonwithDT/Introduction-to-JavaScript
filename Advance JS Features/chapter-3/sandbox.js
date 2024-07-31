d2aa5d31e8aace1e26131a7e95f463daa838ed6d
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-31 22:36:13 +0000

    learning javascript

--- Advance JS Features/chapter-3/sandbox.js
+++ Advance JS Features/chapter-3/sandbox.js
@@ -4,3 +4,11 @@
 //           "I promise to return a vaue"
 //           PENDING -> RESOLVED or REJECTED
 //           new Promise((resolve, reject) => {asynchronous code})
+
+
+// DO THESE CHORES IN ORDER 
+
+// 1. WALK THE DOG 
+// 2. CLEAN THE KITECHEN 
+// 3. TAKE OUT THE TRASH 
+
