eedcb132c8c405a00e76eb77d17e73bfa8b1c4e8
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-31 22:29:59 +0000

    learning javascript

--- Advance JS Features/chapter-3/sandbox.js
+++ Advance JS Features/chapter-3/sandbox.js
@@ -1,1 +1,6 @@
-const name = 'Emmanuel';
+
+// Promise - an object that makes asynchronus operations. 
+//           Wrap a Promise Object around {asynchronous code}
+//           "I promise to return a vaue"
+//           PENDING -> RESOLVED or REJECTED
+//           new Promise((resolve, reject) => {asynchronous code})
