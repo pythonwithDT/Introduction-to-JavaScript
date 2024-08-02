a71b6331fddfa5eebe41007a2f7b53ac1e42259f
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-02 06:19:15 +0000

    learning javascript

---
+++ FULL TUTOROTIAL/Js-booleans and IF/booleans.js
@@ -0,0 +1,7 @@
+// A boolea represnts whether something is true or false
+
+// console.log(3 <5);
+// console.log(3 > 5);
+
+console.log( 5 == '5.00'); // == - tries to convert both numbers into the same type
+console.log( 5 === '5.00'); // === - prevenets the conversion  
