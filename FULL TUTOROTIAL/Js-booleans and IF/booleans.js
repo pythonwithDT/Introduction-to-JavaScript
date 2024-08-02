93a04103cf6a69fcaef98d80fb7482013a0fbda5
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-02 07:00:13 +0000

    learning javascript

--- FULL TUTOROTIAL/Js-booleans and IF/booleans.js
+++ FULL TUTOROTIAL/Js-booleans and IF/booleans.js
@@ -3,5 +3,22 @@
 // console.log(3 <5);
 // console.log(3 > 5);
 
-console.log( 5 == '5.00'); // == - tries to convert both numbers into the same type
-console.log( 5 === '5.00'); // === - prevenets the conversion  
+// console.log( 5 == '5.00'); // == - tries to convert both numbers into the same type
+// console.log( 5 === '5.00'); // === - prevenets the conversion  
+
+// IFs statements 
+
+// if (true) {
+//     console.log('hello');
+// } else {
+//     console.log('hi');
+// }
+
+
+// checking if someone is old enough to drive 
+
+if (15 >= 16) {
+    console.log('You can drive');
+} else {
+    console.log('You cannpt drive');
+}
