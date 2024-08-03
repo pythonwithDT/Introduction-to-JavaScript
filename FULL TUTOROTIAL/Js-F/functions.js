d0d093482da91a4987ce3b46454ae3f025aa1af6
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-03 01:35:51 +0000

    learning javascript

--- FULL TUTOROTIAL/Js-F/functions.js
+++ FULL TUTOROTIAL/Js-F/functions.js
@@ -1,10 +1,17 @@
 // functions 
 
-function function1 () {
-    console.log('hello')
-    console.log(2 + 2)
+// function function1 () {
+//     console.log('hello')
+//     console.log(2 + 2)
+// }
+
+// function1();
+
+// parameters   
+
+function calculateTax(cost, taxPercent = 0.1) {
+    console.log(cost * taxPercent); 
 }
 
-function1();
-
-
+calculateTax(2000, 0.2);
+calculateTax(5000);
