d138c05516c3a369beeb21e5fd582fb0c4c14905
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-06 14:45:42 +0000

    dom-maipulation

---
+++ FULL TUTOROTIAL/JS-OBJ/obj.js
@@ -0,0 +1,16 @@
+// objects 
+
+const product = {
+    name: 'socks',
+    price: 1090
+};
+
+
+console.log(product.price)  // asscessing a value inside an object ( . )
+console.log(product.name); 
+
+product.name = 'cotton socks'  // changes value aasociated with the name property 
+console.log(product);
+
+
+
