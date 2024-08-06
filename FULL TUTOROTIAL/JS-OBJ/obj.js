a569a5579ec9c47aea59e73d95b1a8ea1bb875e3
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-06 15:25:36 +0000

    dom-maipulation

--- FULL TUTOROTIAL/JS-OBJ/obj.js
+++ FULL TUTOROTIAL/JS-OBJ/obj.js
@@ -2,7 +2,11 @@
 
 const product = {
     name: 'socks',
-    price: 1090
+    price: 1090,
+    paste: {
+        cleenteeth: 2,
+        charcoal: 5
+    },
 };
 
 
@@ -11,6 +15,12 @@ console.log(product.name);
 
 product.name = 'cotton socks'  // changes value aasociated with the name property 
 console.log(product);
+
+product.newvalue = 'new value is 50%' // adding a nested object
+
+console.log(product);
+console.log(product.paste)
+console.log(product.paste.cleenteeth);
 
 
 
