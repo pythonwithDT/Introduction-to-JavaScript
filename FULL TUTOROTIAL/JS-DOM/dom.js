5608f196dad012624eb65d565c364f76bd0850bf
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-08 15:04:10 +0000

    DOM

---
+++ FULL TUTOROTIAL/JS-DOM/dom.js
@@ -0,0 +1,23 @@
+
+// DOM
+
+// document.body.innerHTML = "Hello Dear <b>Emefa</b>"
+// document.title = "Am sorry"
+
+
+// properties and methods 
+
+// console.log(document.title);
+
+// console.log(document.body);
+
+
+// console.log(document.body.innerHTML);
+
+// document.body.innerHTML = 'changed'
+// document.body.innerHTML = '<button>Good Job</button>'
+
+
+console.log(document.querySelectorAll('button').innerHTML);
+document.querySelector('button')
+    .innerHTML = 'changed'
