a9866aa6f13e37a0dbe1711ccd4f16149321caa5
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-14 02:07:21 +0000

    Loops and Arrays

---
+++ FULL TUTOROTIAL/Js-Arrays and loops/arrays and lops.js
@@ -0,0 +1,28 @@
+// Array and Loops
+
+[10, 20, 30] // - an array 
+
+const myArray = [10,20,40];
+console.log(myArray)
+
+console.log(myArray[2]) // getting tje last value 
+
+myArray[0] = 99  // changing values in an array 
+console.log(myArray)
+
+
+let array = [1, 'helllo', true, {name:'socks'} [1, 2]]
+console.log(array)
+
+console.log(Array.isArray([1, 'helllo', true, {name:'socks'} [1, 2]]))
+
+
+console.log(myArray.length); // checks for length 
+
+
+myArray.push(60);
+console.log(myArray) // adds a valuen to the end pf an array 
+
+
+myArray.splice(1, 2); // splicing vlaues
+console.log(myArray)
