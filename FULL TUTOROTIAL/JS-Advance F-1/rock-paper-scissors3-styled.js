bc2f31a209a8b8348b4de462087cde28253c55ac
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-28 02:27:15 +0000

    advance functions

--- FULL TUTOROTIAL/JS-Advance F-1/rock-paper-scissors3-styled.js
+++ FULL TUTOROTIAL/JS-Advance F-1/rock-paper-scissors3-styled.js
@@ -62,6 +62,16 @@ document.querySelector('.auto-play-button').addEventListener('click', () => {
   autoPlay();
 })
 
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === "r") {
+    playGame('rock')
+  } else if (event.key === "p") {
+    playGame('paper')
+  } else if (event.key === "s") {
+    playGame('scissors')
+  }
+});
+
 
 function playGame(playerMove) {
   
